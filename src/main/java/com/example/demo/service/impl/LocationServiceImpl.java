326daadd4579f1ed444b33fc3c8562f73cca6315
bala-amypo <bala@amypo.in>
2025-12-15 16:57:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.demo.service.impl;
 
+import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Location;
+import com.example.demo.repository.LocationRepository;
 import com.example.demo.service.LocationService;
 @Service
 public class LocationServiceImpl implements LocationService{
-    Lo
- public Location createLocation(Location, location)
+    @Autowired
+    LocationRepository lrp;
+ public Location createLocation(Location, location){
+    if(location.getLatitude>90){
+        throw new IllegalArgumentException()
+    }
+ }
 }
 public List<Location> getAllLocation(){
 
