3f65ccf8bb1b10e377a328f6003cddb009a19c0c
bala-amypo <bala@amypo.in>
2025-12-15 16:47:32 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationService.java
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import org.hibernate.mapping.List;
+
 import com.example.demo.entity.Location;
 
 public interface LocationService {
-    Location createLocation(Location, location)
+    Location createLocation(Location, location);
+    List<Location> getAllLocation();
 }
