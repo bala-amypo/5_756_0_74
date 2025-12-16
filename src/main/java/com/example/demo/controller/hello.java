5a15a5a5ad5b54ce060421904ea0ace5ac285ef3
bala-amypo <bala@amypo.in>
2025-12-16 13:52:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/hello.java
+++ src/main/java/com/example/demo/controller/hello.java
@@ -1,3 +1,10 @@
  package com.example.demo.controller;
 
- 
+ @RestController
+public class hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
